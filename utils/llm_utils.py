50611065bd44c2e1646fbbda2113faf01ecc0f44
duckinzzz <yurchenko.ps@ya.ru>
2026-01-16 13:40:16 +1000

    changed llm

--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -3,10 +3,10 @@ from groq import AsyncGroq
 from core.bot_core import LLM_TOKEN
 
 client = AsyncGroq(api_key=LLM_TOKEN)
-llm_model_name = "OpenAI/GPT-OSS-120b"
+llm_name = "meta-llama/llama-4-scout-17b-16e-instruct"
 
 system_prompt = (
-    f"Ты — Nerdinzzz 🤓, LLM чат-бот на базе {llm_model_name}. "
+    f"Ты — Nerdinzzz 🤓, LLM чат-бот на базе {llm_name}. "
     "Твой создатель - @duckinzzz. "
     "Ты умеешь быстро отвечать на текстовые сообщения и преобразовывать голосовые в текст. "
     "Отвечай кратко, ясно и по делу, максимум 3-4 предложения. "
@@ -24,8 +24,8 @@ async def get_llm_response(user_prompt: str) -> str:
         {"role": "user", "content": user_prompt}
     ]
 
-    completion = await client.chat.completions.create(model=llm_model_name, messages=messages, temperature=1,
-                                                      max_completion_tokens=8192, top_p=1, reasoning_effort="medium",
+    completion = await client.chat.completions.create(model=llm_name, messages=messages, temperature=1,
+                                                      max_completion_tokens=8192, top_p=1,
                                                       stream=False, stop=None)
 
     return completion.choices[0].message.content
