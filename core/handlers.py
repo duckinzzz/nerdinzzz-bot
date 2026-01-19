3c3eac9daed6c3e5f941031751fbb6f295890781
duckinzzz <yurchenko.ps@ya.ru>
2026-01-19 22:52:11 +1000

    feat: voice note stt

--- core/handlers.py
+++ core/handlers.py
@@ -72,6 +72,21 @@ async def voice_handler(message: types.Message):
         await message.reply(stt_response)
 
 
+@start_router.message(F.content_type == "video_note")
+async def video_note_handler(message: types.Message):
+    video_note = message.video_note
+    file = await bot.get_file(video_note.file_id)
+
+    with tempfile.NamedTemporaryFile(suffix=".mp4") as tmp:
+        await bot.download_file(
+            file.file_path,
+            destination=tmp.name)
+        stt_response = await stt_utils.stt_from_video(tmp.name)
+
+        log_message(message=message, stt_response=stt_response)
+        await message.reply(stt_response)
+
+
 # text + group + mention
 @start_router.message(F.content_type == "text",
                       lambda m: m.chat.type in ("group", "supergroup"),  # группы
--- utils/stt_utils.py
+++ utils/stt_utils.py
@@ -1,3 +1,7 @@
+import tempfile
+
+import ffmpeg
+import imageio_ffmpeg as iio
 from groq import Groq
 
 from core.bot_core import STT_TOKEN
@@ -14,3 +18,17 @@ async def stt(path: str) -> str:
             temperature=0
         )
     return str(result)
+
+
+async def stt_from_video(video_path: str) -> str:
+    ffmpeg_path = iio.get_ffmpeg_exe()
+
+    with tempfile.NamedTemporaryFile(suffix=".wav") as audio_tmp:
+        (
+            ffmpeg
+            .input(video_path)
+            .output(audio_tmp.name, format='wav', acodec='pcm_s16le', ac=1, ar=16000)
+            .run(cmd=ffmpeg_path, overwrite_output=True, quiet=True)
+        )
+        text = await stt(audio_tmp.name)
+    return text
