fdaf82cbf29c330608af8d61c27320f975daa13a
duckinzzz <yurchenko.ps@ya.ru>
2026-02-18 13:33:44 +1000

    misc: react prob 1% -> 0.5%

--- core/constants.py
+++ core/constants.py
@@ -59,6 +59,7 @@ HALLUCINATIONS = {
     'hello everyone',
 }
 
+REACTION_PROB = 0.005
 EMOJIS_RANDOM_POOL = ["🔥", "🤨", "🤡", "🗿", "👻", "🦄"]
 EMOJIS_WEIGHTS = [50, 25, 15, 7, 3, 1]
 EMOJIS_SUPPORTED = ["❤", "👍", "👎", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢",
--- core/middlewares.py
+++ core/middlewares.py
@@ -3,12 +3,12 @@ import random
 from aiogram import BaseMiddleware
 from aiogram.types import reaction_type_emoji
 
-from core.constants import EMOJIS_RANDOM_POOL, EMOJIS_WEIGHTS
+from core.constants import EMOJIS_RANDOM_POOL, EMOJIS_WEIGHTS, REACTION_PROB
 
 
 class LuckyEmojiMiddleware(BaseMiddleware):
     async def __call__(self, handler, message, data):
-        if message.chat.type in ["group", "supergroup"] and random.random() < 0.01:
+        if message.chat.type in ["group", "supergroup"] and random.random() < REACTION_PROB:
             emoji = random.choices(EMOJIS_RANDOM_POOL, weights=EMOJIS_WEIGHTS, k=1)[0]
             await message.react([reaction_type_emoji.ReactionTypeEmoji(emoji=emoji)])
 
